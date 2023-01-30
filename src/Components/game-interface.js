dfac8e543907f963d44cdf2002f01c282dc58812
yl <ylana.ong@gmail.com>
2023-01-31 04:33:07 +0800

    updated timer function

--- src/Components/game-interface.js
+++ src/Components/game-interface.js
@@ -5,17 +5,39 @@ export default function Game() {
     const [time, setTime] = useState(0); 
     const [finished, setFinished] = useState(0);
 
-    function trackTime(startTime) {
-        let elapsed = new Date() - startTime; 
-        setTime(elapsed); 
-     }
+    const elapsedTime = (hrs, mins, secs) => {
+        hrs = cleanTime(hrs);
+        mins = cleanTime(mins); 
+        secs = cleanTime(secs); 
+        return { hrs, mins, secs }
+    }
+   
+    function cleanTime(time){
+        time = time >= 10 ? time : `0${time}`;
+        return time; 
+    }
+
+    let secs = 0 , mins = 0, hrs = 0;  
+    function trackTime() {
+        secs += 1;
+        if(secs == 60){
+            secs = 0; 
+            mins += 1; 
+        }
+        if(mins == 60){
+            mins = 0; 
+            hrs += 1; 
+        }
+        let elTime = (elapsedTime(hrs, mins, secs)); 
+        setTime(`${elTime.hrs}:${elTime.mins}:${elTime.secs}`)
+    }
  
     useEffect(() => {
-        let start = new Date(); 
         if (!finished){
-            setInterval(() => {
-                trackTime(start)
-            }, 1000); 
+            const interval = setInterval(() => trackTime(), 1000); 
+            return () => {
+                clearInterval(interval); 
+            }
         } 
     }, []);
     
